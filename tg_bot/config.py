959e7aae2ea76264642b68dfcb24090b59635dbe
nmk10000 <58908219+nmk10000@users.noreply.github.com>
2020-06-07 14:59:19 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -5,7 +5,7 @@ class Development(Config):
     OWNER_ID = 1181258174  # my telegram ID
     OWNER_USERNAME = "Naeemkk"  # my telegram username
     API_KEY = "1285306305:AAHDIBekL0lOA8tVhoJJRWEAoHdUluT9v6s"  # my api key, as provided by the botfather
-    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/database'  # sample db credentials
+    SQLALCHEMY_DATABASE_URI = 'postgres://pmpkfrsq:7uKhP-2bg61d87vg-ysez12p6gl9YFpJ@ruby.db.elephantsql.com:5432/pmpkfrsq'  # sample db credentials
     MESSAGE_DUMP = '-1234567890' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
     SUDO_USERS = [752438017, 1181258174]  # List of id's for users which have sudo access to the bot.
