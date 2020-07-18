aedc89b597ef9d1ab7a0d1f807b73073b8c773da
nmk10000 <58908219+nmk10000@users.noreply.github.com>
2020-07-18 12:08:17 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -4,7 +4,7 @@ from tg_bot.sample_config import Config
 class Development(Config):
     OWNER_ID = 1181258174  # my telegram ID
     OWNER_USERNAME = "Naeemkk"  # my telegram username
-    API_KEY = "1296488130:AAEkKKXG65eD2vINqCcW8EywugsW1nebB4I"  # my api key, as provided by the botfather
+    API_KEY = "1296488130:AAFWbVyse8ls5xNLhzrnKCkL8kFXekN-mkc"  # my api key, as provided by the botfather
     SQLALCHEMY_DATABASE_URI = 'mysql://nmknmknmk:nmk123@$@nmknmknmk.mysql.pythonanywhere-services.com/nmknmknmk$default'  # sample db credentials
     MESSAGE_DUMP = '-1234567890' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
